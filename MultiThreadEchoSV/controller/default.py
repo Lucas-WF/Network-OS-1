7b46e3c7f2f2b4c7288419a37efa1801fc77869d
Lucas Wagner Fernandes <79675318+Lucas-WF@users.noreply.github.com>
2021-11-25 00:25:26 -0300

    Add files via upload

--- MultiThreadEchoSV/controller/default.py
+++ MultiThreadEchoSV/controller/default.py
@@ -1,32 +1,21 @@
 from view.tkinter_app import Application
-from models import server, imc, client
+from models.client import Client
 
 
 class Controller:
-
     def __init__(self):
         self.view = Application(self)
-        self.server = server.Server()
-        self.client = client.Client()
-        self.server.bind()
-        self.server.listen()
+        self.client = Client()
         self.client.connect()
-        self.server.accept()
-        self.server.authorized_conn()
-
-    def dict_search(self):
-        for i, x in self.server.datadict.items():
-            if i == self.server.listconn[1]:
-                peso, idade, altura = x
-                return peso, idade, altura
-
-    def send_msg(self):
-        self.client.send_thread(self.view.get_entry())
-        self.server.thread_recve()
-        self.model = imc.IMC(*self.dict_search())
-        result = self.model.calc()
-        self.server.thread_send(result)
-        return self.client.recve()
 
     def run(self):
         self.view.main_v()
+
+    def control_send(self):
+        self.client.send(self.view.get_entry())
+
+    def control_recv(self):
+        return self.client.recve()
+
+    def close(self):
+        self.client.close()
